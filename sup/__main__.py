530365f7fbf7cb989af1eebc28f6264f6cda4cc5
Enoki <77694265+EnokiUN@users.noreply.github.com>
2021-11-18 18:35:22 +0300

    Update __main__.py

--- sup/__main__.py
+++ sup/__main__.py
@@ -1,6 +1,6 @@
 """MIT License
 
-Copyright (c) 2021-Present EnokiUN
+Copyright (c) 2021-present EnokiUN
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
