53cb3364ca030aa9c1af372b8873f32c7f9bf869
Enoki <77694265+EnokiUN@users.noreply.github.com>
2021-11-21 12:14:39 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'SUP'
+project = 'PSUP'
 copyright = '2021, EnokiUN'
 author = 'EnokiUN'
 
