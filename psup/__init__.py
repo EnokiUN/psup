509ebb15c0e4567a6406a342e27169f75be827cf
Enoki <77694265+EnokiUN@users.noreply.github.com>
2021-11-23 18:38:34 +0300

    Update __init__.py

--- psup/__init__.py
+++ psup/__init__.py
@@ -35,7 +35,7 @@ __title__ = 'PSUP'
 __author__ = 'EnokiUN'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2021-present EnokiUN'
-__version__ = '0.1.2.3'
+__version__ = '0.3.4'
 
 __all__ = ["Story", "StoryError", "OnlineStory"]
 
