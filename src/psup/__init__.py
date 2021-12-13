cbefffd1d398a3afd97bdbce2f22ce6527929c6b
Vincent <vincentbusiness55@gmail.com>
2021-12-12 19:19:01 -0800

    Fix

--- src/psup/__init__.py
+++ src/psup/__init__.py
@@ -38,8 +38,8 @@ __license__ = "MIT"
 __copyright__ = "Copyright (c) 2021-present EnokiUN"
 __version__ = "0.4.0-rc1"
 
-__all__ = ["Story", "StoryError", "OnlineStory"]
-
 from .errors import StoryError
 from .onlinestory import OnlineStory
 from .story import Story
+
+__all__ = ["Story", "StoryError", "OnlineStory"]
