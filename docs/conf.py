1865b845b19f585f1fca0d2248337575a35bd659
Vincent <vincentbusiness55@gmail.com>
2022-01-09 22:28:28 +0800

    Attribute 2022 copyright

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 # -- Project information -----------------------------------------------------
 
 project = "PSUP"
-copyright = "2021, EnokiUN"
+copyright = "2021-2022, EnokiUN"
 author = "EnokiUN"
 
 # The full version, including alpha/beta/rc tags
